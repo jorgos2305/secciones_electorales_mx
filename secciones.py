c30cd3c52c7699079a2ebfb903ffe84e9e8f7e91
jorgos2305 <jorge.tellez.rosales@outlook.com>
2025-03-02 11:23:18 +0100

    Add the documentation of the functions

--- secciones.py
+++ secciones.py
@@ -3,13 +3,14 @@ import geopandas as gpd
 from pathlib import Path
 
 def load_secciones_of(state:Path) -> gpd.GeoDataFrame:
-    """_summary_
+    """This function loads the GeoDataFrame from the .shp files. This DataFrame does not contain the names of the municipios, only their number and the number
+    of the secciones.
 
     Args:
-        state (Path): _description_
+        state (Path): .shp files downloaded from the INE
 
     Returns:
-        gpd.GeoDataFrame: _description_
+        gpd.GeoDataFrame: The GeoDataFrame containing the geometry of the secciones
     """
     secciones = gpd.read_file(state)
     secciones.columns = [col.lower() for col in secciones.columns]
@@ -17,16 +18,17 @@ def load_secciones_of(state:Path) -> gpd.GeoDataFrame:
     secciones['id_municipio'] = secciones['municipio']
     secciones = secciones.drop(['id', 'entidad', 'distrito_f', 'distrito_l', 'tipo', 'control', 'geometry1_'], axis=1)
     secciones = secciones.loc[:,['id_municipio', 'municipio', 'seccion', 'geometry']]
+    # This transform the geometry data into altitude and latitude so it can be opened with Google earth
     return secciones.to_crs('EPSG:4326')
 
 def load_municipio_of(state:Path) -> gpd.GeoDataFrame:
-    """_summary_
+    """This function get the names of the municipios from the .xls files.
 
     Args:
-        state (Path): _description_
+        state (Path): .xls files downloaded from the INE
 
     Returns:
-        pd.DataFrame: _description_
+        pd.GeoDataFrame: The DataFrame only contains the names of the minucipios and their id.
     """
     if state.suffix not in ('.xls', '.xlsx'):
         raise TypeError("You did not enter a valid excel file")
@@ -35,15 +37,25 @@ def load_municipio_of(state:Path) -> gpd.GeoDataFrame:
     return municipios.drop(['entidad'], axis=1)
 
 def assign_seccion_to_municipio(secciones:gpd.GeoDataFrame, municipios:gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    """This function combines the secciones and municipios DataFrame.
+
+    Args:
+        secciones (gpd.GeoDataFrame): DataFrame with the secciones' geometry
+        municipios (gpd.GeoDataFrame): DataFrame with the names of the municipios
+
+    Returns:
+        gpd.GeoDataFrame: Final DataFrame to be exported
+    """
     gdf = gpd.GeoDataFrame(pd.merge(municipios, secciones, left_on='municipio', right_on='municipio'))
     gdf = gdf.loc[:, ['seccion', 'nombre entidad', 'id_municipio', 'nombre municipio', 'geometry']]
     return gdf.rename(columns={'seccion':'id'})
 
 def export_to_geojson(secciones_by_municipio:gpd.GeoDataFrame) -> str:
-    """_summary_
+    """Exports the secciones files divided by municipio, in this way, the files are separated by municipios in Google earth
+    and inside of each miunicipio are the secciones.
 
     Args:
-        secciones_by_municipio (gpd.GeoDataFrame): _description_
+        secciones_by_municipio (gpd.GeoDataFrame): Final DataFrame to be exported.
     """
     municipio_dict = dict(zip(secciones_by_municipio['id_municipio'].values, secciones_by_municipio['nombre municipio'].values))
     state_name = secciones_by_municipio['nombre entidad'].unique()
@@ -56,6 +68,7 @@ def export_to_geojson(secciones_by_municipio:gpd.GeoDataFrame) -> str:
     return str(storage_location)
 
 def define_storage_location(state_name:str) -> Path:
+    """Helper function to defined the location where the files must be stored."""
     return Path(__file__).parent.joinpath(state_name)
 
 if __name__ == '__name__':
